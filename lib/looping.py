e6f869bd9c7491e0452b41c6d024a1fd6f3806bd
bettayub <bettayub19@gmail.com>
2023-08-18 15:33:05 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,48 @@
-#!/usr/bin/env python3
-
+# happyNewYear() function
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
+happy_new_year()
+print()
+
+# fizzbuzzPrinter() and fizzbuzz() functions
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
+
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+fizzbuzz_printer()
+print()
+
+# reverseString() function
+def reverse_string(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
+
+input_string = input("Enter a string: ")
+reversed_input = reverse_string(input_string)
+print("Reversed string:", reversed_input)
+print()
+
+# square_integers() function
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
-def fizzbuzz():
-    # code goes here!
-    pass
+input_list = [int(x) for x in input("Enter a list of integers separated by spaces: ").split()]
+squared_list = square_integers(input_list)
+print("Squared integers:", squared_list)
